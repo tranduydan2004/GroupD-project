6c07b66ff960eb7f7d4f9d1870c115b72d77321b
QuangDung1204 <quangdung12042004@gmail.com>
2024-09-25 23:54:29 +0700

    Your commit message

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import { Outlet, Routes } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 import Header from './components/header'
 import Footer from './components/Footer'
 
 function App() {
 
   return (
-    <Routes>
+    <>
       <Header />
-      <Footer />
       <Outlet />
-    </Routes>
+      <Footer />
+    </>
+
   )
 }
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
     return (
-        <footer className="bg-white text-black py-4 text-center fixed bottom-0 left-0 w-full">
+        <footer className="bg-white text-black py-4 text-center bottom-0 left-0 w-full">
             <p>© 2024 Công ty của bạn. Mọi quyền được bảo lưu.</p>
         </footer>
     )
